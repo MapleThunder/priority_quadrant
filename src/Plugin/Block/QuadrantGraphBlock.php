8fffd0b68062cedab9147214a40f249220cb2377
Niko Bentley <niko.bentley@redspace.com>
2021-09-08 16:10:55 -0300

    uadded template file for graph block

--- src/Plugin/Block/QuadrantGraphBlock.php
+++ src/Plugin/Block/QuadrantGraphBlock.php
@@ -3,6 +3,9 @@
 namespace Drupal\priority_quadrant\Plugin\Block;
 
 use Drupal\Core\Block\BlockBase;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Drupal\Core\Routing\RouteMatchInterface;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
 
 /**
  * Provides a block to display the graph from a Priority Grouping.
@@ -13,13 +16,65 @@ use Drupal\Core\Block\BlockBase;
  *   category = @Translation("Organization"),
  * )
  */
-class QuadrantGraphBlock extends BlockBase {
+class QuadrantGraphBlock extends BlockBase implements ContainerFactoryPluginInterface {
+
+  /**
+   * The route match object.
+   *
+   * @var \Drupal\Core\Routing\RouteMatchInterface
+   */
+  protected $routeMatch;
+
+  /**
+   * Constructs a QuadrantGraphBlock object.
+   *
+   * @param array $configuration
+   *   The default configuration.
+   * @param string $plugin_id
+   *   The plugin id.
+   * @param string $plugin_definition
+   *   The plugin definition.
+   * @param \Drupal\Core\Routing\RouteMatchInterface $routeMatch
+   *   The route match object.
+   */
+  public function __construct(
+    array $configuration,
+    $plugin_id,
+    $plugin_definition,
+    RouteMatchInterface $routeMatch
+  ) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
+    $this->routeMatch = $routeMatch;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(
+    ContainerInterface $container,
+    array $configuration,
+    $plugin_id,
+    $plugin_definition
+  ) {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get("current_route_match")
+    );
+  }
 
   /**
    * {@inheritdoc}
    */
   public function build() {
-    // @todo Implement build() method.
+    $node = $this->routeMatch->getParameter('node');
+    dump($node);
+
+    return [
+      "#theme" => 'graph-block',
+      "#data" => [],
+    ];
   }
 
 }
